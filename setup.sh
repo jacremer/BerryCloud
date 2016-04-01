83fc04c12a77b63a08c16e6adf3948ae966a92c5
ezraholm50 <ezraholm50@gmail.com>
2016-04-01 23:55:44 +0100

    Letsencrypt is stable as long as you forward port 443 to your pi machine's LAN IP

--- setup.sh
+++ setup.sh
@@ -575,25 +575,26 @@ echo "RewriteCond %{HTTPS} off" >> $OCPATH/.htaccess
 echo "RewriteRule (.*) https://%{HTTP_HOST}%{REQUEST_URI} [R,L]" >> $OCPATH/.htaccess
 
 clear
+
 # Let's Encrypt
-#function ask_yes_or_no() {
-#    read -p "$1 ([y]es or [N]o): "
-#    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
-#        y|yes) echo "yes" ;;
-#        *)     echo "no" ;;
-#    esac
-#}
-#if [[ "yes" == $(ask_yes_or_no "Do you want to install a real SSL cert (from Let's Encrypt) on this machine?
-#The scripts are still Beta, feel free to contribute!") ]]
-#then
-#	bash $SCRIPTS/activate-ssl.sh
-#else
-#	echo
-#    	echo "OK, but if you want to run it later, just type: sudo bash $SCRIPTS/activate-ssl.sh"
-#    	echo -e "\e[32m"
-#    	read -p "Press any key to continue... " -n1 -s
-#    	echo -e "\e[0m"
-#fi
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [N]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+if [[ "yes" == $(ask_yes_or_no "Do you want to install a real SSL cert (from Let's Encrypt) on this machine?
+The scripts are still Beta, feel free to contribute!") ]]
+then
+	bash $SCRIPTS/activate-ssl.sh
+else
+	echo
+    	echo "OK, but if you want to run it later, just type: sudo bash $SCRIPTS/activate-ssl.sh"
+    	echo -e "\e[32m"
+    	read -p "Press any key to continue... " -n1 -s
+    	echo -e "\e[0m"
+fi
 
 # Option to set Dynamic Dns updates
 bash $SCRIPTS/dyndns.sh
