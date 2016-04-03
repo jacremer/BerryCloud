08454e4cbee19536cca8393ec53f705c023982d6
Ezra <ezraholm50@gmail.com>
2016-04-03 18:55:38 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,2 +1,13 @@
 ##BerryCloud version 1.0.1 stable
 
+## Version 1.0.2
+apt-get update && apt-get upgrade -y && apt-get -f install -y
+apt-get install rsyslog module-init-tools
+dpkg --configure --pending
+
+# Install rpi-update
+sudo curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
+echo "sudo rpi-update" >> /etc/cron.weekly/rpi-update.sh
+chmod 755 /etc/cron.weekly/rpi-update.sh
+rpi-update
+reboot
