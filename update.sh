6e91d75d2b7b2c4358fd3cc844651297853508d4
Ezra <ezraholm50@gmail.com>
2016-04-04 19:27:46 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -47,17 +47,6 @@ echo "rm /var/scripts/update_checker.sh" >> /etc/cron.daily/fresh_update_checker
 echo "wget https://github.com/ezraholm50/BerryCloud/raw/master/update_checker.sh -P /var/scripts/" > /etc/cron.daily/fresh_update_checker.sh
 chmod 750 /etc/cron.daily/fresh_update_checker.sh
 
-# Install rpi-update
-echo "deb http://archive.raspberrypi.org/debian/ jessie main" > /etc/apt/sources.list
-wget "http://archive.raspberrypi.org/debian/raspberrypi.gpg.key"
-apt-key add raspberrypi.gpg.key
-apt-get update
-apt-get install libraspberrypi-bin -y
-curl -L --output /usr/bin/rpi-update https://raw.githubusercontent.com/Hexxeh/rpi-update/master/rpi-update && sudo chmod +x /usr/bin/rpi-update
-echo "rpi-update" >> /etc/cron.weekly/rpi-update.sh
-rpi-update
-chmod 750 /etc/cron.daily/rpi-update.sh
-
 # Get the latest login screen
 rm /var/scripts/techandme.sh
 wget https://github.com/ezraholm50/BerryCloud/raw/master/techandme.sh -P /var/scripts/
