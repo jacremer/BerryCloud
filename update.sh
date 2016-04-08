a6f50ce6f6b97e2efe76a832b0f5d8e91f9e6d8c
Ezra <ezraholm50@gmail.com>
2016-04-08 08:49:29 +0100

    Update update.sh

--- update.sh
+++ update.sh
@@ -133,6 +133,7 @@ chmod 750 /var/scripts/techandme.sh
 # Cleanup
 rm /var/scripts/update.sh
 apt-get autoclean -y && apt-get autoremove -y && apt-get update && apt-get upgrade -y
+update-grub
 reboot
 
 ##################################################################################################################################################
